9d1370bbae88df55fbd9a54ef996b735894de7bb
said magdy <saidmagdy511@gmail.com>
2026-02-10 13:04:52 +0200

    feature/ add signup controller logic

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./Routes/auth.route.js";
 import messagesRoutes from "./Routes/message.route.js";
 
-dotenv.config();
-
+dotenv.config(); //to read .env file and set environment variables
 const app = express();
+
+app.use(express.json()); // to read json data from request body
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRoutes);
@@ -13,4 +15,5 @@ app.use("/api/messages", messagesRoutes);
 const port = process.env.Port || 3000;
 app.listen(port, () => {
   console.log(`server running in port ${port}`);
+  connectDB();
 });
